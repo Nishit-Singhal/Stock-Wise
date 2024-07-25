d6b03cdfb10fac8c89dd55b92ff8cc6904add7be
sparky0520 <132695189+sparky0520@users.noreply.github.com>
2024-07-25 20:56:28 +0530

    Stock Wise

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
 import './index.css'
 import Navbar from "./Navbar.jsx"
-import Market from "./Market.jsx"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <Navbar/>
